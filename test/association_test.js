9730ac1fce6ef96a19dd1c1d0604d521e352188d
niamurrell <niamurrell@users.noreply.github.com>
2017-09-21 19:55:06 -0700

    Access deep-nested associations

--- test/association_test.js
+++ test/association_test.js
@@ -11,7 +11,7 @@ describe("Associations:", () => {
   beforeEach((done) => {
     joe = new User({ name: "Joe" });
     blogPost = new BlogPost({ title: "JS is hard", content: "But it's ok, you'll get there" });
-    comment = new Comment({ content: "Yeah you'll get the hang of it! "});
+    comment = new Comment({ content: "Yeah you'll get the hang of it!"});
 
     // Once the data has been set, identify it as necessary with references
     joe.blogPosts.push(blogPost);
@@ -31,4 +31,26 @@ describe("Associations:", () => {
         done();
       });
   });
+
+  it("saves a full relation graph", (done) => {
+    User.findOne({ name: "Joe" })
+      .populate({
+        path: "blogPosts",
+        populate: {
+          path: "comments",
+          model: "comment",
+          populate: {
+            path: "user",
+            model: "user"
+          }
+        }
+      })
+      .then((user) => {
+        assert(user.name === "Joe");
+        assert(user.blogPosts[0].title === "JS is hard");
+        assert(user.blogPosts[0].comments[0].content === "Yeah you'll get the hang of it!");
+        assert(user.blogPosts[0].comments[0].user.name === "Joe");
+        done();
+      });
+    });
 });
